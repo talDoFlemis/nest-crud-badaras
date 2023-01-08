622eb6910dada56fb3e7aa84fb97bb9358ee76ee
taldoflemis <coderflemis@gmail.com>
2023-01-08 16:57:36 -0300

    add josias controller unit tests

--- src/josias/josias.controller.spec.ts
+++ src/josias/josias.controller.spec.ts
@@ -1,9 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { UpdateJosiaDto } from './dto/update-josia.dto';
+import { Josias4Real } from './entities/josia.entity';
 import { JosiasController } from './josias.controller';
 import { JosiasService } from './josias.service';
 
 describe('JosiasController', () => {
   let controller: JosiasController;
+  let service: JosiasService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,9 +15,30 @@ describe('JosiasController', () => {
     }).compile();
 
     controller = module.get<JosiasController>(JosiasController);
+    service = module.get<JosiasService>(JosiasService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('findJosias', () => {
+    const result = new Josias4Real();
+    jest.spyOn(service, 'findJosias').mockImplementation(() => result);
+    expect(controller.findJosias()).toBe(result);
+  });
+
+  it('update', () => {
+    const result = new Josias4Real();
+    result.mood = 'serious';
+    jest.spyOn(service, 'update').mockImplementation(() => result);
+
+    const mood = new UpdateJosiaDto();
+    mood.mood = 'serious';
+
+    expect(controller.update(mood)).toBe(result);
+  });
+
+  it('delete', () => {
+    const res = 'a random string';
+    jest.spyOn(service, 'remove').mockImplementation(() => res);
+
+    expect(controller.remove()).toBe(res);
   });
 });
