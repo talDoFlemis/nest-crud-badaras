b85eed7eb93e106cccc28b047fd55ae78481e297
taldoflemis <coderflemis@gmail.com>
2023-01-08 16:57:18 -0300

    removing old app files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { JosiasModule } from './josias/josias.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [JosiasModule],
 })
 export class AppModule {}
