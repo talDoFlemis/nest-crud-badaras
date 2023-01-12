1c99a2946d6f9cf4a5c5d6cc4c6c7de59b348f85
taldoflemis <coderflemis@gmail.com>
2023-01-12 13:53:58 -0300

    adding typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { JosiasModule } from './josias/josias.module';
 
 @Module({
-  imports: [JosiasModule],
+  imports: [
+    JosiasModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'flemis',
+      password: '',
+      database: 'tubias',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
