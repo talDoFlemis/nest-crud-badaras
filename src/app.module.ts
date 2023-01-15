6f273b65d7446a2403c8a4e3260f0a2f14966734
taldoflemis <coderflemis@gmail.com>
2023-01-15 09:10:06 -0300

    feat: add config module
    add nest/config dependencie
    
    add .env to gitignore
    
    add database.config
    
    add env validation
    
    add .env.example
    
    add ConfigModule to AppModule
    
    add AsyncConfig injection for TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import databaseConfig from 'config/database.config';
+import { validate } from 'config/env.validation';
+import { DataSourceOptions } from 'typeorm';
 import { JosiasModule } from './josias/josias.module';
 import { TobaianorModule } from './tobaianor/tobaianor.module';
 
 @Module({
   imports: [
     JosiasModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'flemis',
-      password: '',
-      database: 'tubias',
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
     TobaianorModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        return configService.get<DataSourceOptions>('database');
+      },
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      load: [databaseConfig],
+      validate,
+    }),
   ],
 })
 export class AppModule {}
